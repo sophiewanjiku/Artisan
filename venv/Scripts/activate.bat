c5636a0e58d3e7d32694e19fd1392e4a407fd1b0
kariuki sophia <skwanjiku@kabarak.ac.ke>
2024-03-03 23:19:04 +0300

    merge frontend updates

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Lorraine\Documents\Artisan\venv
+set VIRTUAL_ENV=C:\Users\TEST\Downloads\ECommerceSite-Django\ECommerceSite-Django\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
